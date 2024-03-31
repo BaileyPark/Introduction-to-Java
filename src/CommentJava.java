fbe8d1d395cfba9c9c7fae40edc1714371b67f71
WorkaHolic <baileypark@kakao.com>
2024-04-01 03:54:35 +0900

    Hello World

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hellow java1");
+//        System.out.println("hellow java2");
+
+        /*
+        System.out.println("hellow java3");
+        System.out.println("hellow java4");
+        */
+    }
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("hellow java1");
+        System.out.println("hellow java2");
+        System.out.println("hellow java3");
+    }
+}
