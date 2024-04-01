c69781b233f97448427fedd08303764fcf672414
WorkaHolic <baileypark@kakao.com>
2024-04-02 02:24:42 +0900

    Opercation

---
+++ src/variable/ex/operator/ex/OperationEx2.java
@@ -0,0 +1,14 @@
+package variable.ex.operator.ex;
+
+public class OperationEx2 {
+    public static void main(String[] args) {
+        double var1 = 1.5;
+        double var2 = 2.5;
+        double var3 = 3.5;
+     
+        double result = var1 + var2 + var3;
+        System.out.println("result = " + result);
+        System.out.println("result = " + result/3);
+        
+    }
+}
